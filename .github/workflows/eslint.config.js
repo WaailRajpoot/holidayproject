a196846aa884ad54e5e66835b50a0f5cd28db964
Waail Rajpoot <waailrajpoot99@gmail.com>
2024-12-31 02:26:49 +0500

    Added a config file

---
+++ .github/workflows/eslint.config.js
@@ -0,0 +1,11 @@
+// eslint.config.js
+module.exports = [
+    {
+      files: ['**/*.js'], // Match your file types
+      rules: {
+        'no-unused-vars': 'warn', // Example: Warn for unused variables
+        'no-console': 'off', // Example: Allow console statements
+      },
+    },
+  ];
+  
